7c994e4db6547ff15025e7e061d786a53ca68ecd
Edvin Saletovic <saletovic.developer@gmail.com>
2022-06-20 20:07:10 -0500

    test all subtotals

--- src/pages/entry/ToppingOptions.js
+++ src/pages/entry/ToppingOptions.js
@@ -1,6 +1,10 @@
 import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
 
-export default function ToppingOption({ name, imagePath }) {
+export default function ToppingOption({ name, imagePath, updateItemCount }) {
+  const handleChange = (e) => {
+    updateItemCount(name, e.target.checked ? 1 : 0);
+  };
   return (
     <Col xs={12} sm={6} md={4} lg={3} className="text-center">
       <img
@@ -8,6 +12,14 @@ export default function ToppingOption({ name, imagePath }) {
         alt={`${name} topping`}
         src={`http://localhost:3030/${imagePath}`}
       />
+      <Form.Group controlId={`${name}-topping-checkbox`}>
+        <Form.Check
+          onChange={handleChange}
+          data-testid={`${name}-topping-test-id`}
+          type="checkbox"
+          label={name}
+        />
+      </Form.Group>
     </Col>
   );
 }
--- src/pages/entry/test/TotalUpdates.test.js
+++ src/pages/entry/test/TotalUpdates.test.js
@@ -25,3 +25,39 @@ test("update scoop subtotal when it changes", async () => {
   userEvent.type(chocolateInput, "2");
   expect(scoopsSubtotal).toHaveTextContent("6.00");
 });
+
+test("update toppings subtota when checked", async () => {
+  render(<Options optionType={"toppings"} />, {
+    wrapper: OrderDetailsProvider,
+  });
+
+  // total starts at 0
+  const toppingsSubtotal = screen.getByText("Toppings total: $", {
+    exact: false,
+  });
+  expect(toppingsSubtotal).toHaveTextContent("0.00");
+
+  // click/update/check Cherries topping an check subtotal
+  const cherrieCheckmark = await screen.findByTestId(
+    "Cherries-topping-test-id"
+  );
+  expect(cherrieCheckmark).not.toBeChecked();
+  userEvent.click(cherrieCheckmark);
+  expect(toppingsSubtotal).toHaveTextContent("1.50");
+
+  // click/update/check M&Ms topping and check subtotal
+  const mmCheckmark = await screen.findByTestId("M&Ms-topping-test-id");
+  expect(mmCheckmark).not.toBeChecked();
+  userEvent.click(mmCheckmark);
+  expect(toppingsSubtotal).toHaveTextContent("3.00");
+
+  // click/update/check Hot Fudge topping and check subtotal
+  const fudgeCheckmark = await screen.findByTestId("Hot Fudge-topping-test-id");
+  expect(fudgeCheckmark).not.toBeChecked();
+  userEvent.click(fudgeCheckmark);
+  expect(toppingsSubtotal).toHaveTextContent("4.50");
+
+  // click/update/UN-CHECK Cherries topping and check subtotal
+  userEvent.click(cherrieCheckmark);
+  expect(toppingsSubtotal).toHaveTextContent("3.00");
+});
